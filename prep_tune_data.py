5f8e5148179b658f1cd75bbaa236d4162971df41
Louie Cervantes <louiecervantes@wvsu.edu.ph>
2024-04-16 13:29:21 +0800

    Save

--- prep_tune_data.py
+++ prep_tune_data.py
@@ -59,6 +59,7 @@ def app():
     with open("data/aleanon-sentences.jsonl", "w") as f:
         f.write(jsonl_str)
 
+    st.write("The JSONL file has been saved. Verifying if it is a valid JSONL file...")
     # verifying if the file is a valid jsonl file
     filepath = "data/aleanon-sentences.jsonl"
     if is_valid_jsonl(filepath):
